d7c71f8191e2bcea6a957e3c996c148d31c45ad9
faizan102418 <m.faizansajid102418@gmail.com>
2026-05-24 15:19:10 +0500

    Day 1: add Transaction model and initial migration

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    category = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
